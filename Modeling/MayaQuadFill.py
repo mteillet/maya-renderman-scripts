3a91a0d2cb48720785fab3975a8aef80dcfd5f4c
Martin Teillet <martin.teillet@hotmail.fr>
2019-08-26 04:03:09 +0200

    Update MayaQuadFill.py
    Managed to store each quarter in a separate list.
    Still need to first define halves and then having quarter defined from halves in order for the script to work on multiplier of 2 edge loops

--- Modeling/MayaQuadFill.py
+++ Modeling/MayaQuadFill.py
@@ -11,7 +11,13 @@ edgesSelected = cmds.ls(selection = True, shortNames = True, flatten = True)
 curentObject = cmds.ls(selection = True, objectsOnly = True, shortNames = True)
 #Get the number of selected edges
 edgesNumber = cmds.polyEvaluate(edgeComponent = True)
+#Quartes of the edge loop
+firstQuarter = []
+secondQuarter = []
+thirdQuarter = []
+fourthQuarter = []
 #while loop
+quarterLoop = 0
 whileLoop = 0
 n = 0
 
@@ -23,48 +29,48 @@ print(edgesNumber)
 #### Need to store the quarters as separate list in order to refer to individual edges at the extermities of the list in an easier way
 #### Should also allow to implement the rotate a lot more easier
 
-#Check if number is even
+
+#Store the 4 quarters of the edgeloop
+#Find the quarter size
 if edgesNumber % 2 == 0:
-    #Calculates the number of half edges
-    edgesHalf = edgesNumber / 2
-    #Calculates the number of quarter edges
-    edgesQuarter = edgesHalf / 2
-    print(edgesHalf, edgesQuarter)
+    quarterSize = edgesNumber / 4
+    print(quarterSize)
+    #Sets the list of edges containing the four quarters
+    while quarterLoop < quarterSize:
+        firstQuarter.append(edgesSelected[quarterLoop])
+        secondQuarter.append(edgesSelected[(quarterLoop) + (quarterSize)])
+        thirdQuarter.append(edgesSelected[(quarterLoop) + ( (quarterSize) * 2 )])
+        fourthQuarter.append(edgesSelected[(quarterLoop) + ( (quarterSize) * 3 )])
+
+        quarterLoop += 1
+    print(firstQuarter)
+    print(secondQuarter)
+    print(thirdQuarter)
+    print(fourthQuarter)
+# if not an even number, error
 else :
-    #Calculates the number of half edges
-    edgesHalf = (edgesNumber - 1)/2
-    #Calculates the number of quarter edges
-    edgesQuarter = edgesHalf / 2
-    print("Edge loop is an odd number of edges")
-    print(edgesHalf, edgesQuarter)
+    print("ERROR")
 
 
+#Deselect second and fourth quarters 
+cmds.select((secondQuarter), toggle = True)
+cmds.select((fourthQuarter), toggle = True)
 
-#While loop deselect quarters of edges
-while whileLoop <= edgesQuarter:
-    print ("N is:")
-    print(n)
-    #Deselect the first quarter of the loop
-    cmds.select(((edgesSelected[n])), toggle = True)
-    print(edgesSelected[n])
+#Finds the number of divisions needed based on the quarter size
+divisionsNeeded = quarterSize - 3
+currentSelection = cmds.ls(selection = True, shortNames = True)
+cmds.polyBridgeEdge(currentSelection, divisions = (divisionsNeeded))
 
-    #Deselect the opposite quarter of the loop
-    m = n + edgesHalf
-    cmds.select((str(edgesSelected[m])), toggle = True)
-
-    #increments
-    n += 1
-    whileLoop += 1
+print (divisionsNeeded)
 
 
+# Need to find a way to connect the quarters second and fourth to the new creater faces on the bridge
+# Possible to remove first and last edges in the indexes of quarters second and fourth in order to bridge correctly
+# Try to select edge loops connected to the quarters, and then remove the first and last index of the quarter before bridging
 
+# Would be even better if one loop was taken away from every quarter list in order to be able to find the division needed in a more natural way
+# Would also allow to spin the quarters in a much easier manner
 
-#Get the number of remaining edges
-remainingEdges = edgesNumber - edgesHalf 
-#Calculates the number of divisions needed for the first bridge
-divisionNeeded = remainingEdges - 7
-
-# Bridge the opposing loop quarters
-# Get selection first
-firstQuarters = cmds.ls(selection = True, shortNames = True)
-cmds.polyBridgeEdge(firstQuarters, divisions = (divisionNeeded))
+# Need to make the script work on A mutiplier of 2 (currently only of 4)
+# Should be fixed by rewriting the way quarters are defined
+    # First split edge loop in halves, and then split each half into two quarters
