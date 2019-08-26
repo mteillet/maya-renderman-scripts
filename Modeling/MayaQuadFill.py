f1a537a66adc05181d79f04f91c6845b7593b442
Martin Teillet <martin.teillet@hotmail.fr>
2019-08-26 03:12:12 +0200

    Update MayaQuadFill.py
    issue with the length of the edges index
    the n incrementing the loop refers to an out of index value

--- Modeling/MayaQuadFill.py
+++ Modeling/MayaQuadFill.py
@@ -11,12 +11,59 @@ edgesSelected = cmds.ls(selection = True, shortNames = True)
 curentObject = cmds.ls(selection = True, objectsOnly = True, shortNames = True)
 #Get the number of selected edges
 edgesNumber = cmds.polyEvaluate(edgeComponent = True)
+#while loop
+whileLoop = 0
+n = 0
 
 
 print(curentObject)
 print(edgesSelected)
 print(edgesNumber)
 
+#Check if number is even
+if edgesNumber % 2 == 0:
+    #Calculates the number of half edges
+    edgesHalf = edgesNumber / 2
+    #Calculates the number of quarter edges
+    edgesQuarter = edgesHalf / 2
+    print(edgesHalf, edgesQuarter)
+else :
+    #Calculates the number of half edges
+    edgesHalf = (edgesNumber - 1)/2
+    #Calculates the number of quarter edges
+    edgesQuarter = edgesHalf / 2
+    print("Edge loop is an odd number of edges")
+    print(edgesHalf, edgesQuarter)
+
+
+#While loop deselect quarters of edges
+while whileLoop <= edgesQuarter:
+    #Deselect the first quarter of the loop
+    cmds.select(((edgesSelected[n])), toggle = True)
+    print(edgesSelected[n])
+
+    #Deselect the opposite quarter of the loop
+    m = n + edgesHalf
+    #cmds.select((str(edgesSelected[m])), toggle = True)
+
+    #increments
+    n += 1
+    whileLoop += 1
+
+"""
+
+
+#Get the number of remaining edges
+remainingEdges = edgesNumber - edgesHalf 
+#Calculates the number of divisions needed for the first bridge
+divisionNeeded = remainingEdges - 6
+
+# Bridge the opposing loop quarters
+# Get selection first
+firstQuarters = cmds.ls(selection = True, shortNames = True)
+cmds.polyBridgeEdge(firstQuarters, divisions = (divisionNeeded))
+
 
 #Deselecting the specified edges
 
+"""
