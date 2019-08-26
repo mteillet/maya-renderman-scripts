08056dcfab2cc4b0af81aa9fcb159226ec3b6e65
Martin Teillet <martin.teillet@hotmail.fr>
2019-08-26 03:24:06 +0200

    Update MayaQuadFill.py
    Need to store the quarters as separate list in order to refer to individual edges at the extermities of the list in an easier way.
    Should also allow to implement the rotate a lot more easier

--- Modeling/MayaQuadFill.py
+++ Modeling/MayaQuadFill.py
@@ -6,7 +6,7 @@ import maya.mel as mel
 
 #   Selected Variables
 #Edges
-edgesSelected = cmds.ls(selection = True, shortNames = True)
+edgesSelected = cmds.ls(selection = True, shortNames = True, flatten = True)
 #Object
 curentObject = cmds.ls(selection = True, objectsOnly = True, shortNames = True)
 #Get the number of selected edges
@@ -19,6 +19,9 @@ n = 0
 print(curentObject)
 print(edgesSelected)
 print(edgesNumber)
+
+#### Need to store the quarters as separate list in order to refer to individual edges at the extermities of the list in an easier way
+#### Should also allow to implement the rotate a lot more easier
 
 #Check if number is even
 if edgesNumber % 2 == 0:
@@ -36,34 +39,32 @@ else :
     print(edgesHalf, edgesQuarter)
 
 
+
 #While loop deselect quarters of edges
 while whileLoop <= edgesQuarter:
+    print ("N is:")
+    print(n)
     #Deselect the first quarter of the loop
     cmds.select(((edgesSelected[n])), toggle = True)
     print(edgesSelected[n])
 
     #Deselect the opposite quarter of the loop
     m = n + edgesHalf
-    #cmds.select((str(edgesSelected[m])), toggle = True)
+    cmds.select((str(edgesSelected[m])), toggle = True)
 
     #increments
     n += 1
     whileLoop += 1
 
-"""
+
 
 
 #Get the number of remaining edges
 remainingEdges = edgesNumber - edgesHalf 
 #Calculates the number of divisions needed for the first bridge
-divisionNeeded = remainingEdges - 6
+divisionNeeded = remainingEdges - 7
 
 # Bridge the opposing loop quarters
 # Get selection first
 firstQuarters = cmds.ls(selection = True, shortNames = True)
 cmds.polyBridgeEdge(firstQuarters, divisions = (divisionNeeded))
-
-
-#Deselecting the specified edges
-
-"""
