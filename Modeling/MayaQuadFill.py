f88ac8429255b39652e593a1b95e25b24cc36a2d
Martin Teillet <martin.teillet@hotmail.fr>
2019-08-27 04:54:13 +0200

    Update MayaQuadFill.py
    Need to redefine the way divisions are calculated (works well with 4 and 5, but not with 1, 2, 3, 6, 7, etc...)

--- Modeling/MayaQuadFill.py
+++ Modeling/MayaQuadFill.py
@@ -1,14 +1,15 @@
 import maya.cmds as cmds
 import maya.mel as mel
+import re
 
 ####    This script fills the selected edge loop with quads     ####
-
-
-### Variables
 
 ####################################################
 ####                MAYA WINDOW                 ####
 ####################################################
+
+originalSelection = cmds.ls(orderedSelection=True, flatten=True)
+originaldivision = len(originalSelection)/8
 
 
 GridFill = cmds.window( title="Grid Fill", iconName='Grid Fill', widthHeight=(140, 90), toolbox = True)
@@ -16,7 +17,7 @@ cmds.rowColumnLayout( numberOfColumns=2, columnWidth = [(1,75),(2,60)])
 cmds.text( label="Offset" )
 offset = cmds.intField(changeCommand = 'userInput()', value = 0)
 cmds.text( label="Divisions" )
-divisions = cmds.intField(changeCommand = 'userInput()', value = 1)
+divisions = cmds.intField(changeCommand = 'userInput()', value = originaldivision)
 
 cmds.separator(h=20, style = "none")
 cmds.separator(h=20, style = "none")
@@ -53,7 +54,9 @@ def gridFill():
 
     #Get short name of current object for the polySelect fonction
     shortObject = str(curentObject).replace("Shape","").replace("u'","").replace("'","").replace("[","").replace("]","")
-
+    firstBridgeBorder = []
+    secondBridgeBorder = []
+    i = 0
 
 
     #Ordering the number of edges
@@ -65,93 +68,73 @@ def gridFill():
     print("This is the new edges order following the offset " + str(ReorderedEdges))
     
     #Splitting the list in 2
-    FirstHalf = ReorderedEdges[:len(ReorderedEdges)//2]
-    SecondHalf = ReorderedEdges[len(ReorderedEdges)//2:]
+    firstHalf = ReorderedEdges[:len(ReorderedEdges)//2]
+    secondHalf = ReorderedEdges[len(ReorderedEdges)//2:]
     
-    print(FirstHalf)
-    print(SecondHalf)
+    print("firstHalf")
+    print((firstHalf))
+    print("firstBridgeBorder")
+    
+    firstBridgeBorder = firstHalf
+    secondBridgeBorder = secondHalf
+    thirdBridgeBorder = []
+    fourthBridgeBorder = []
 
+    # Removes from the bridges border the number of divisions needed from both bridge borders
+    # Also resets i at the end of while loop
 
-    """
+    while i < userDivision:
+        #Get the last items which will be unselected in order to make the last bridges easier
+        thirdBridgeBorder = [firstBridgeBorder[len(firstBridgeBorder)-1], firstBridgeBorder[0]]
+        del firstBridgeBorder[len(firstBridgeBorder)-1]
+        del firstBridgeBorder[0]
+        i += 1
 
-    print(curentObject)
-    print(edgesSelected)
-    print(edgesNumber)
+    i = 0
 
-    #### Need to store the quarters as separate list in order to refer to individual edges at the extermities of the list in an easier way
-    #### Should also allow to implement the rotate a lot more easier
+    while i < userDivision:
+        #Get the last items which will be unselected in order to make the last bridges easier
+        fourthBridgeBorder = [secondBridgeBorder[len(secondBridgeBorder)-1], secondBridgeBorder[0]]  
+        del secondBridgeBorder[len(secondBridgeBorder)-1]
+        del secondBridgeBorder[0]
+        i += 1
 
-
-    #Store the 4 quarters of the edgeloop
-    #Find the quarter size
-    if edgesNumber % 2 == 0:
-        quarterSize = edgesNumber / 4
-        print(quarterSize)
-
-
-
-        #Sets the list of edges containing the four quarters
-        while quarterLoop < quarterSize:
-            firstQuarter.append(edgesSelected[quarterLoop])
-            secondQuarter.append(edgesSelected[(quarterLoop) + (quarterSize)])
-            thirdQuarter.append(edgesSelected[(quarterLoop) + ( (quarterSize) * 2 )])
-            fourthQuarter.append(edgesSelected[(quarterLoop) + ( (quarterSize) * 3 )])
-
-            quarterLoop += 1
-        print(firstQuarter)
-        print(secondQuarter)
-        print(thirdQuarter)
-        print(fourthQuarter)
-        #Get rid of every first and last edges of indexes for bridges sides
-        #The removed edges are still in the quarter lists
-        firstBridgeSide = firstQuarter[:-1]
-        secondBridgeSide = secondQuarter[:-1]
-        thirdBridgeSide = thirdQuarter[:-1]
-        fourthBridgeSide = fourthQuarter[:-1]
+    i = 0
+    
+    
+    cmds.select(clear = True)
+    cmds.select(firstBridgeBorder, secondBridgeBorder, add = True)
+    
+    if userDivision % 2 == 0:
+        cmds.polyBridgeEdge(divisions = (userDivision+1))
         
-
-    # if not an even number, error
-    else :
-        print("ERROR")
+    else:
+        cmds.polyBridgeEdge(divisions = (userDivision+2))
 
 
 
-    #Select first and third bride sides 
+
+    ##############################
+    #Filling the last two bridges#
+    ##############################
+    
     cmds.select(clear = True)
-    cmds.select((firstBridgeSide), toggle = True)
-    cmds.select((thirdBridgeSide), toggle = True)
-
-
-
-
-    #Finds the number of divisions needed based on the quarter size
-    #Bridges the first two Bridges sides
-    divisionsNeeded = quarterSize - 2
-    currentSelection = cmds.ls(selection = True, shortNames = True)
-    cmds.polyBridgeEdge(currentSelection, divisions = (divisionsNeeded))
-
-    #Select the second Bridge edge 
+    cmds.select(thirdBridgeBorder[0], add = True)
+    
+    cmds.polySelectSp(loop=True)
+    
+    cmds.select(thirdBridgeBorder[0], fourthBridgeBorder[1], add = True, toggle = True)
+    
+    cmds.polyBridgeEdge(divisions=0)
+    
+        
     cmds.select(clear = True)
-    cmds.select((secondBridgeSide), toggle = True)
-
-    #select the new edge border next to the second bridge
-    print(secondBridgeSide)
-    secondEdgeBorder= str(secondBridgeSide[0])
-    print(secondEdgeBorder)
-
-    #cmds.polySelect(shortObject, edgeBorder=1316)
-
-
-    # Need to find a way to connect the quarters second and fourth to the new creater faces on the bridge
-    # Possible to remove first and last edges in the indexes of quarters second and fourth in order to bridge correctly
-    # Try to select edge loops connected to the quarters, and then remove the first and last index of the quarter before bridging
-
-    # Need to make the script work on A mutiplier of 2 (currently only of 4)
-    # Should be fixed by rewriting the way quarters are defined
-        # First split edge loop in halves, and then split each half into two quarters
-
-    """
-
-
-if __name__ == "__main__":
-	main()
+    cmds.select(thirdBridgeBorder[1], add = True)
+    
+    cmds.polySelectSp(loop=True)
+    
+    cmds.select(thirdBridgeBorder[1], fourthBridgeBorder[0], add = True, toggle = True)
+    
+    cmds.polyBridgeEdge(divisions=0)
+    
+#### Need to redefine the way divisions are calculated (works well with 4 and 5, but not with 1, 2, 3, 6, 7, etc...)
